504341a245b9cffe776ce63a232aa25d8d0881b9
Jim <jimmontour@gmail.com>
2020-12-10 17:56:02 -0500

    Added contact page

--- src/contact.js
+++ src/contact.js
@@ -4,9 +4,29 @@ function loadContact() {
     container.innerHTML = '';
     const contact = document.createElement('div');
 
-    contact.innerHTML = `<p>Add contact content here!</p>`
+    contact.innerHTML = `
+        <h1>Contact Jim's Diner</h1>
+        <form class="contact-form" action="">
+            <div class="contact-form-section">
+                <p>Name:</p>
+                <input type="text">
+            </div>
+            <div class="contact-form-section">
+                <p>Phone:</p>
+                <input type="text">
+            </div>
+            <div class="contact-form-section">
+                <p>Email:</p>
+                <input type="email">
+            </div>
+            <div class="contact-form-section">
+                <p>Comment:</p>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+            </div>
+        </form>
+    `;
 
     container.appendChild(contact);
 }
 
-export default loadContact;
+export default loadContact;4
--- src/home.js
+++ src/home.js
@@ -4,9 +4,9 @@ function loadHome() {
     const emptyDiv = container.innerHTML = '';
     const home = document.createElement('div');
     home.innerHTML = `
-        <div class="home">
-            <h1>Welcome to Jim's Cafe</h1> 
-            <img src="https://source.unsplash.com/random/" alt="">
+        <div class="home">    
+        <h1>Welcome to Jim's Cafe</h1> 
+            <img src="/img/hero-image.jpg" alt="">
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero earum voluptas necessitatibus quisquam repellendus quo.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora eligendi consequuntur asperiores nesciunt natus delectus! Quos, quibusdam officiis? Ex, sunt totam! Explicabo nam at sed?</p>
         </div>
